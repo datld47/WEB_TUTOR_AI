e06c061a8ee0570adacb81504066768ced49e36d
dat-sys <datbmt198747@gmail.com>
2025-06-29 18:20:27 +0700

    branch-dat-002-V2.0.1

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # branch-dat-002
 ## V2.0
 - create project feeeback
+
+## V2.0.1
+- get / post
+
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 # Create your views here.
 def review(request):
+    if request.method=='POST':
+        entered_username=request.POST['username']
+        print(entered_username)
+        #return render(request,'reviews/thank-you.html')
+        return HttpResponseRedirect("/thank-you")
     return render(request,"reviews/review.html")
+
+def thank_you(request):
+    return render(request,'reviews/thank-you.html')
